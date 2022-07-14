cff7108a80b5d8c39ec2083cada8db516c6110b7
imthemobin <imthemobin@gmail.com>
2022-07-14 23:56:48 +0430

    step 1

---
+++ todo/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/app/modules/home/view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'TODO app',
+      home:  Home(),
+    );
+  }
+}
+
